895cd808d36e389818ed4de1bbb190ebe9b244d0
Kaio Ferreira <121730814+KaioFerr@users.noreply.github.com>
2024-08-21 21:30:38 -0300

    Update Exercicio05.cpp

--- Exercicio05.cpp
+++ Exercicio05.cpp
@@ -12,17 +12,20 @@ string defineCategory(int swimmerAge) {
     if (swimmerAge < 7) {
         return "Categoria: Infantil A";
     }
-    if ( swimmerAge > 7 && swimmerAge <= 10) {
+    else if ( swimmerAge > 7 && swimmerAge <= 10) {
         return "Categoria: Infantil B";
     }
-    if (swimmerAge > 10 && swimmerAge <= 13) {
+    else if (swimmerAge > 10 && swimmerAge <= 13) {
         return "Categoria: Juvenil A";
     }
-    if (swimmerAge > 13 && swimmerAge <= 17) {
+    else if (swimmerAge > 13 && swimmerAge <= 17) {
         return "Categoria: Juvenil B";
     }
-    if (swimmerAge > 17) {
+    else if (swimmerAge > 17) {
         return "Categoria: Adulto";
+    }
+    else {
+        return "Idade nao informada";
     }
 }
 
